6d8ae2a12a1b4f2d03cae82abb8e2ac3d2433b46
suhanabolt <106386090+suhanabolt@users.noreply.github.com>
2022-06-07 13:10:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var first_name='suhana';
+console.log(first_name);
+var last_name='shaik';
+console.log(last_name)
+var fullname=first_name.concat(last_name)
+console.log(fullname)
+var a=10;
+var b=15;
+var add=a+b;
+console.log(add)
+var sub=a-b;
+console.log(sub)
+var mul=a*b;
+console.log(mul)
+var div=a/b;
+console.log(div)
